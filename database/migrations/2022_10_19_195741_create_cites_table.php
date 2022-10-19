3f5e24aac041a4b74ed5d36ba9f517266c6645eb
Alansan <alaansannchezz@gmail.com>
2022-10-19 17:01:20 -0300

    feat: Creando ModelFactoryMigration to Cite

---
+++ database/migrations/2022_10_19_195741_create_cites_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cites', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('surname', 60);
+            $table->string('phone_number', 10);
+            $table->date('date');
+            $table->time('time');
+            $table->unsignedBigInteger('barber_id');
+            $table->foreign('barber_id')
+                ->references('id')
+                ->on('barbers')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cites');
+    }
+};
