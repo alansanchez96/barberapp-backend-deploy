02612d6c170b5317a3447cf14a4f9f294e3e4646
Alansan <alaansannchezz@gmail.com>
2022-10-19 16:13:26 -0300

    feat: Columnas añadidas a tabla <barbers>

--- database/migrations/2022_10_19_185721_create_barbers_table.php
+++ database/migrations/2022_10_19_185721_create_barbers_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('barbers', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 60);
+            $table->string('surname', 60);
+            $table->enum('status', [0, 1])->default(1);
             $table->timestamps();
         });
     }
